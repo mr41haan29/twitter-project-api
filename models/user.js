1bba138f753c635585f4c8d318358eeed9303376
Mohammad S Raihaan <raihaan129@gmail.com>
2024-10-06 12:30:07 -0400

    creating user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,24 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
-    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minLength: 6 },
+    profileImage: { type: String },
+    email: { type: String, required: true, unique: true },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
   },
   {
     timestamps: true,
